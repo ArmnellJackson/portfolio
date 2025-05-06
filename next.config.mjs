779b208870f7bdde69c0b90c5d23bd49e4765fc4
Jackson <armnellgomez@gmail.com>
2025-05-05 22:39:10 -0500

    modificamos contenido front-end-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Outputs a Single-Page Application (SPA)
-  distDir: 'build', // Changes the build output directory to `build`
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
